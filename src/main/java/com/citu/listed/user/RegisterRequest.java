01037fe7afda4f945fffcb42f1f77c58243048c4
jlambojo <lambojojezelle@gmail.com>
2023-08-23 20:36:38 +0800

    added @NotEmpty annotation

--- src/main/java/com/citu/listed/user/RegisterRequest.java
+++ src/main/java/com/citu/listed/user/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.citu.listed.user;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotEmpty(message = "Name is required.")
     private String name;
+
+    @NotEmpty(message = "Username is required.")
     private String username;
+
+    @NotEmpty(message = "Password is required.")
     private String password;
 }
