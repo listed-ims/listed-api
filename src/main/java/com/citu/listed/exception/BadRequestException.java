1902b1910f23dfffac1ece492cf15fee0ed572f3
jlambojo <lambojojezelle@gmail.com>
2023-08-23 14:43:40 +0800

    added bad request exception handler

---
+++ src/main/java/com/citu/listed/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.citu.listed.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
