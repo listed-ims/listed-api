62f61ec25433d83b29b0fbf27aac09e0c311b104
Dave Roy L. Capalaran <126816093+dr-cplrn@users.noreply.github.com>
2023-08-26 16:59:30 +0800

    Added get user end point

--- src/main/java/com/citu/listed/user/UserController.java
+++ src/main/java/com/citu/listed/user/UserController.java
@@ -2,6 +2,7 @@ package com.citu.listed.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,4 +33,14 @@ public class UserController {
     public ResponseEntity<Object> validateUsername(@RequestParam String username){
         return new ResponseEntity<>(service.validateUsername(username), HttpStatus.OK);
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<Object> getUser(@RequestHeader HttpHeaders headers) {
+
+        String token = headers.getFirst(HttpHeaders.AUTHORIZATION).substring(7);
+        UserResponse userResponse = service.getUser(token);
+
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/citu/listed/user/UserService.java
+++ src/main/java/com/citu/listed/user/UserService.java
@@ -1,8 +1,13 @@
 package com.citu.listed.user;
 
+import com.citu.listed.product.ProductResponse;
+
 public interface UserService {
 
    AuthenticationResponse register(RegisterRequest request);
    AuthenticationResponse authenticate(AuthenticationRequest request);
    boolean validateUsername(String username);
+   UserResponse getUser(String token);
+
+
 }
