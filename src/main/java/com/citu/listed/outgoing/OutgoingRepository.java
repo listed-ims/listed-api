ca6a4f65c9a5db70213f60499935c90965109a5f
Faith Rosalijos <faithrosalijos@gmail.com>
2023-09-26 11:28:31 +0800

    Added missing imports

--- src/main/java/com/citu/listed/outgoing/OutgoingRepository.java
+++ src/main/java/com/citu/listed/outgoing/OutgoingRepository.java
@@ -4,6 +4,8 @@ import com.citu.listed.outgoing.enums.OutgoingCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Repository
