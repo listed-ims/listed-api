1cc4129506417df947fbcb06291f86d4161e437b
jlambojo <lambojojezelle@gmail.com>
2023-10-13 11:44:01 +0800

    modified token expiration

--- src/main/java/com/citu/listed/user/config/JwtServiceImplementation.java
+++ src/main/java/com/citu/listed/user/config/JwtServiceImplementation.java
@@ -48,7 +48,7 @@ public class JwtServiceImplementation implements JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
