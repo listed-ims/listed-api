e7042f8da026162588538243dc9517aec66ed05c
Jemwel Odo <odojemwel@gmail.com>
2023-10-31 06:53:40 +0800

    removed HttpServletRequest in custom method security implementation

--- src/main/java/com/citu/listed/user/security/MethodSecurityService.java
+++ src/main/java/com/citu/listed/user/security/MethodSecurityService.java
@@ -2,26 +2,18 @@ package com.citu.listed.user.security;
 
 import com.citu.listed.permission.enums.UserPermissions;
 import com.citu.listed.user.User;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.Objects;
 
 @Service("MethodSecurity")
 @RequiredArgsConstructor
 public class MethodSecurityService {
     Authentication authentication;
-    HttpServletRequest request;
 
     public boolean hasPermission(String permission) {
-        request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
-                .getRequest();
         authentication = SecurityContextHolder.getContext().getAuthentication();
 
         User user = (User) authentication.getPrincipal();
@@ -35,14 +27,12 @@ public class MethodSecurityService {
     }
 
     public boolean hasAnyPermission(String... permissions) {
-        request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
-                .getRequest();
         authentication = SecurityContextHolder.getContext().getAuthentication();
 
         User user = (User) authentication.getPrincipal();
 
         SimpleGrantedAuthority ownerAuthority = new SimpleGrantedAuthority(user.getCurrentStoreId() + "_" + UserPermissions.OWNER);
-        
+
         for (String permission : permissions) {
             SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getCurrentStoreId() + "_" + permission);
 
